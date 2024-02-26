e74f7063727ede57be078f9061f59cc6588661c9
Cuongbui1203 <cuongbuimanh1203@gmail.com>
2024-02-26 15:52:01 +0700

    them chuyen doi ngon ngu

---
+++ resources/js/Components/LanguageSwitcher.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import i18n from '@/i18n'; // Import i18n instance
+
+function LanguageSwitcher() {
+    const [locale, setLocale] = useState(i18n.language);
+
+    const handleChange = (event) => {
+        i18n.changeLanguage(event.target.value);
+        setLocale(event.target.value);
+        localStorage.setItem('lang', event.target.value);
+        window.location.reload(false);
+    };
+
+    return (
+        <select value={locale} onChange={handleChange}>
+            <option value="vi">Tiếng Việt</option>
+            <option value="en">English</option>
+        </select>
+    );
+}
+
+export default LanguageSwitcher;
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
-
+import i18n from './i18n'; // Import i18n instance
+import { I18nextProvider } from 'react-i18next';
 const appName =
     window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -12,7 +13,12 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => require(`./Pages/${name}`),
     setup({ el, App, props }) {
-        return render(<App {...props} />, el);
+        return render(
+            <I18nextProvider i18n={i18n}>
+                <App {...props} />
+            </I18nextProvider>,
+            el
+        );
     },
 });
 
