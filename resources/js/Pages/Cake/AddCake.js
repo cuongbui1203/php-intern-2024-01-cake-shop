c57b9368686e28656e5eedbd86ad62f8dccda5a3
Cuongbui1203 <cuongbuimanh1203@gmail.com>
2024-03-26 15:44:51 +0700

    cap nhat giao dien moi cho them banh moi ra lo

--- resources/js/Pages/Cake/AddCake.js
+++ resources/js/Pages/Cake/AddCake.js
@@ -1,13 +1,12 @@
 import Input from '@/Components/Input';
 import Label from '@/Components/Label';
-import Navbar from '@/Components/Navbar';
 import Title from '@/Components/Title';
 import i18n from '@/i18n';
-import { Button } from 'antd';
+import { Button, InputNumber } from 'antd';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-export default function AddCake({ auth, cake }) {
+export default function AddCake({ id, name, onOk, onCancel }) {
     const [t] = useTranslation();
     const [amount, setAmount] = useState(0);
     const submit = async () => {
@@ -16,7 +15,7 @@ export default function AddCake({ auth, cake }) {
         };
         const res = await axios.post(
             route('api.cakes.addCake', {
-                cake: cake.id
+                cake: id
             }),
             data,
             {
@@ -25,33 +24,46 @@ export default function AddCake({ auth, cake }) {
                 }
             }
         );
-        location.href = '/admin/cakes';
+        onOk();
     };
     return (
-        <>
-            <Navbar auth={auth} />
+        <div className=" h-[13rem]">
             <Title title={t('addCake')} />
             <div className="d-flex flex-column w-50 h-20 m-auto">
+                <div className="mb-[5px]">
+                    {t('Name')}: {name}
+                </div>
                 <Label
                     forInput="amount"
                     value={t('AddAmount')}
                     className="m-2"
                 />
-                <Input
-                    type="text"
+                <InputNumber
                     name="amount"
                     value={amount}
                     className="mt-1 block w-full m-2"
                     isFocused={true}
-                    handleChange={(e) => {
-                        setAmount(e.target.value);
+                    min={0}
+                    onChange={(e) => {
+                        setAmount(e);
                     }}
                     required
                 />
-                <Button onClick={submit} className="m-auto">
-                    {t('ok')}
-                </Button>
+                <div className="w-full flex justify-between mt-2">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded"
+                        onClick={submit}
+                    >
+                        {t('ok')}
+                    </button>
+                    <button
+                        onClick={onCancel}
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded"
+                    >
+                        {t('Cancel')}
+                    </button>
+                </div>
             </div>
-        </>
+        </div>
     );
 }
