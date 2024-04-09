b82559da985893664ab530527f496521e50fe30f
Kuon <cuongbuimanh1203@gmail.com>
2024-04-09 13:04:53 +0700

    Tao test case cho Modle Cake

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'rating' => rand(1, 5),
+            'comment' => $this->faker->text(),
+        ];
+    }
+}
